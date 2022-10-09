192154c9d421ea1af2851327c8e9b7b875cd9631
Daniel Keane <info@alphabetsigns.com>
2022-10-09 11:35:12 -0400

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQkoq9cAZwNcEj-9VSA1xqX7zgGPPfvXo",
-  authDomain: "fir-recipes-75f0d.firebaseapp.com",
-  projectId: "fir-recipes-75f0d",
-  storageBucket: "fir-recipes-75f0d.appspot.com",
-  messagingSenderId: "249298872364",
-  appId: "1:249298872364:web:7d8bfc102bcd6ed5ea8989",
+
 };
 
 const app = initializeApp(firebaseConfig);
